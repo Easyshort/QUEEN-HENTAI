2eb96faba5f300a78a084b7bc05b3872fa2e88b3
Easyshort <95911622+Easyshort@users.noreply.github.com>
2024-02-17 23:47:35 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94767939688', 'Dinuwa Official²⁰²³', true],
-  ['94767939688', 'Dinuwa Official²⁰²³', true], 
+  ['08067962350', 'Dinuwa Official²⁰²³', true],
+  ['08067962350', 'Dinuwa Official²⁰²³', true], 
   [''] 
 ] //Number of owners
 
